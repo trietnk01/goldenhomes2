83602b7f28fb2cd1612580b58518f2a7403d4e67
NguyenKimDien <trietnk01@gmail.com>
2018-12-24 11:31:08 +0700

    cập nhật

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'ketoanvuongphat');
+define('DB_NAME', 'goldenhomes2');
 
 /** MySQL database username */
 define('DB_USER', 'root');
