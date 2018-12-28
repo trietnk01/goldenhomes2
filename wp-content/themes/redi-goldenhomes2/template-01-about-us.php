5732dcc13bc8554500e562cfdf89ba56df2c47ce
NguyenKimDien <trietnk01@gmail.com>
2018-12-28 18:00:48 +0700

    cập nhật

--- wp-content/themes/redi-goldenhomes2/template-01-about-us.php
+++ wp-content/themes/redi-goldenhomes2/template-01-about-us.php
@@ -40,10 +40,38 @@ get_header();
 						<div class="truc-tuyen">Trực tuyến</div>
 					</div>
 					<div class="support-hotline">
-						<span class="support-phone-icon"></span>
-						<span class="support-phone-number"></span>
+						<div class="support-phone-icon"><img src="<?php echo P_IMG.'/telephone.png'; ?>"></div>
+						<div class="support-phone-number"><a href="javascript:void(0);">0903 600 002</a></div>
+						<div class="clr"></div>
+					</div>
+					<div class="call-skype-viber-zalo">
+						Call: Skype - Viber - Zalo 
+					</div>
+					<div class="cara-call-skype-viber-zalo">
+						<img src="<?php echo P_IMG.'/skype-viber-zalo.png'; ?>">
+					</div>
+					<div class="support-email">
+						Email: nguyenbaotuoc@gmail.com
 					</div>
 				</div>
+				<h3 class="cac-dich-vu-chinh">Các dịch vụ chính</h3>
+				<div class="main-service">
+					<div class="dvc" style="background-image: url('<?php echo P_IMG.'/banner_1.png'; ?>')">
+						<h3><a href="javascript:void(0);">Thiết kế nội thất</a></h3>
+					</div>
+					<div class="dvc" style="background-image: url('<?php echo P_IMG.'/banner_2.png'; ?>')">
+						<h3><a href="javascript:void(0);">Thi công nội thất</a></h3>
+					</div>
+					<div class="dvc" style="background-image: url('<?php echo P_IMG.'/banner_3.png'; ?>')">
+						<h3><a href="javascript:void(0);">Sản phẩm rèm cửa</a></h3>
+					</div>
+					<div class="dvc" style="background-image: url('<?php echo P_IMG.'/banner_4.png'; ?>')">
+						<h3><a href="javascript:void(0);">Giấy gián tường</a></h3>
+					</div>
+					<div class="dvc" style="background-image: url('<?php echo P_IMG.'/banner_5.png'; ?>')">
+						<h3><a href="javascript:void(0);">Giàn phơi thông minh</a></h3>
+					</div>
+				</div>				
 			</div>
 		</div>
 	</div>
