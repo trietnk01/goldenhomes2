643e374f0671d42202f69b3b36d13af4a7c69f86
NguyenKimDien <trietnk01@gmail.com>
2018-12-28 16:46:52 +0700

    cập nhật

--- wp-content/themes/redi-goldenhomes2/template-01-about-us.php
+++ wp-content/themes/redi-goldenhomes2/template-01-about-us.php
@@ -5,22 +5,47 @@ Template Post Type: post, page
 */
 get_header(); 
 ?>
-<div class="container">
-	<div class="row">
-		<div class="col-lg-9">
-			<h2 class="single-title">
-				Giới thiệu
-			</h2>
-			<div class="single-content">
-				<p>Golden Homes là 1 công ty nội thất có tuổi đời hơn 5 năm kinh nghiệm, phát triển với đội ngũ hơn 50 nhân sự và sẽ còn nhiều hơn thế, Hiện là thương hiệu thiết kế kiến trúc, nội thất nổi tiếng tại Việt Nam, Với sự nỗ lực trong suốt nhiều năm qua, Golden Homes  mong muốn mang lại cho khách hàng những sản phẩm thiết kế tốt nhất và biến những sản phẩm thiết kế đó trở thành hiện thực. Golden Homes  tin rằng với khát vọng đầy tính nhân văn của mình, sẽ tạo được nhiều giá trị thích dụng và bền vững cho toàn xã hội </p>
+<div class="box-single">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">			
+				<h2 class="single-title">
+					Giới thiệu
+				</h2>
+				<div class="single-content">
+					<p>Golden Homes là 1 công ty nội thất có tuổi đời hơn 5 năm kinh nghiệm, phát triển với đội ngũ hơn 50 nhân sự và sẽ còn nhiều hơn thế, Hiện là thương hiệu thiết kế kiến trúc, nội thất nổi tiếng tại Việt Nam, Với sự nỗ lực trong suốt nhiều năm qua, Golden Homes  mong muốn mang lại cho khách hàng những sản phẩm thiết kế tốt nhất và biến những sản phẩm thiết kế đó trở thành hiện thực. Golden Homes  tin rằng với khát vọng đầy tính nhân văn của mình, sẽ tạo được nhiều giá trị thích dụng và bền vững cho toàn xã hội </p>
+					<p><img src="<?php echo P_IMG.'/cai-ban.png'; ?>"></p>
 
-				<p>Sau khi hoàn thành bản vẽ, Golden Homes tiến hành lập dự toán thi công. Để đảm bảo chi phí thi công được kiểm soát chặt chẽ, Golden Homes tiến hành đề xuất vật liệu, màu sắc, chủng loại phụ kiện, vải rèm, giấy dán tường,.... và bắt đầu vào giai đoạn thi công ngoài công trường. Golden Homes luôn có giám sát thường trực có mặt tại  công trình, để kiểm tra chặt chẽ quy trình và các vướng mắc
-				Golden Homes là 1 công ty nội thất có tuổi đời hơn 5 năm kinh nghiệm, phát triển với đội ngũ hơn 50 nhân sự và sẽ còn nhiều hơn thế, Hiện là thương hiệu thiết kế kiến trúc, nội thất nổi tiếng tại Việt Nam, Với sự nỗ lực trong suốt nhiều năm qua, Golden Homes  mong muốn mang lại cho khách hàng những sản phẩm thiết kế tốt nhất và biến những sản phẩm thiết kế đó trở thành hiện thực. Golden Homes  tin rằng với khát vọng đầy tính nhân văn của mình, sẽ tạo được nhiều giá trị thích dụng và bền vững cho toàn xã hội</p>
+					<p>Sau khi hoàn thành bản vẽ, Golden Homes tiến hành lập dự toán thi công. Để đảm bảo chi phí thi công được kiểm soát chặt chẽ, Golden Homes tiến hành đề xuất vật liệu, màu sắc, chủng loại phụ kiện, vải rèm, giấy dán tường,.... và bắt đầu vào giai đoạn thi công ngoài công trường. Golden Homes luôn có giám sát thường trực có mặt tại  công trình, để kiểm tra chặt chẽ quy trình và các vướng mắc
+					Golden Homes là 1 công ty nội thất có tuổi đời hơn 5 năm kinh nghiệm, phát triển với đội ngũ hơn 50 nhân sự và sẽ còn nhiều hơn thế, Hiện là thương hiệu thiết kế kiến trúc, nội thất nổi tiếng tại Việt Nam, Với sự nỗ lực trong suốt nhiều năm qua, Golden Homes  mong muốn mang lại cho khách hàng những sản phẩm thiết kế tốt nhất và biến những sản phẩm thiết kế đó trở thành hiện thực. Golden Homes  tin rằng với khát vọng đầy tính nhân văn của mình, sẽ tạo được nhiều giá trị thích dụng và bền vững cho toàn xã hội</p>
 
-				<p>Sau khi hoàn thành bản vẽ, Golden Homes tiến hành lập dự toán thi công. Để đảm bảo chi phí thi công được kiểm soát chặt chẽ, Golden Homes tiến hành đề xuất vật liệu, màu sắc, chủng loại phụ kiện, vải rèm, giấy dán tường,.... và bắt đầu vào giai đoạn thi công ngoài công trường. Golden Homes luôn có giám sát thường trực có mặt tại  công trình, để kiểm tra chặt chẽ quy trình và các vướng mắc</p>
+					<p>Sau khi hoàn thành bản vẽ, Golden Homes tiến hành lập dự toán thi công. Để đảm bảo chi phí thi công được kiểm soát chặt chẽ, Golden Homes tiến hành đề xuất vật liệu, màu sắc, chủng loại phụ kiện, vải rèm, giấy dán tường,.... và bắt đầu vào giai đoạn thi công ngoài công trường. Golden Homes luôn có giám sát thường trực có mặt tại  công trình, để kiểm tra chặt chẽ quy trình và các vướng mắc</p>
+					<p>Golden Homes là 1 công ty nội thất có tuổi đời hơn 5 năm kinh nghiệm, phát triển với đội ngũ hơn 50 nhân sự và sẽ còn nhiều hơn thế, Hiện là thương hiệu thiết kế kiến trúc, nội thất nổi tiếng tại Việt Nam, Với sự nỗ lực trong suốt nhiều năm qua, Golden Homes  mong muốn mang lại cho khách hàng những sản phẩm thiết kế tốt nhất và biến những sản phẩm thiết kế đó trở thành hiện thực. Golden Homes  tin rằng với khát vọng đầy tính nhân văn của mình, sẽ tạo được nhiều giá trị thích dụng và bền vững cho toàn xã hội</p>
+					<p>
+					Sau khi hoàn thành bản vẽ, Golden Homes tiến hành lập dự toán thi công. Để đảm bảo chi phí thi công được kiểm soát chặt chẽ, Golden Homes tiến hành đề xuất vật liệu, màu sắc, chủng loại phụ kiện, vải rèm, giấy dán tường,.... và bắt đầu vào giai đoạn thi công ngoài công trường. Golden Homes luôn có giám sát thường trực có mặt tại  công trình, để kiểm tra chặt chẽ quy trình và các vướng mắc</p>
+				</div>
+				<div class="single-thong-tin-chi-tiet">
+					<div class="dbtttct">Để biết thêm thông tin chi tiết, xin vui lòng liên hệ với chúng tôi tại:</div>
+					<div class="goldenhomes">Golden Homes</div>
+					<div class="dc-dt-em"><span class="golden-left"><i class="fa fa-map-marker" aria-hidden="true"></i></span>&nbsp;&nbsp;<span class="golden-right">42/1 Đường 17, P. Hiệp Bình Chánh, Q. Thủ Đức, TP.HCM</span></div>
+					<div class="dc-dt-em"><span class="golden-left"><i class="fa fa-phone" aria-hidden="true"></i></span>&nbsp;&nbsp;<span class="golden-right">0903 600 002</span> </div>
+					<div class="dc-dt-em"><span class="golden-left"><i class="fa fa-envelope" aria-hidden="true"></i></span>&nbsp;&nbsp;<span class="golden-right">nguyenbaotuoc@gmail.com</span></div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="support-online-box">
+					<img src="<?php echo P_IMG.'/ho-tro-truc-tuyen.png'; ?>">
+					<div class="httt">
+						<div class="ho-tro">Hỗ trợ</div>
+						<div class="truc-tuyen">Trực tuyến</div>
+					</div>
+					<div class="support-hotline">
+						<span class="support-phone-icon"></span>
+						<span class="support-phone-number"></span>
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="col-lg-3"></div>
 	</div>
 </div>
 <?php
