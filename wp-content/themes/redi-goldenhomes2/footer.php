98259b82473e614c21e5f1efd9c798f65b0dae95
NguyenKimDien <trietnk01@gmail.com>
2018-12-28 13:52:36 +0700

    cap nhat

--- wp-content/themes/redi-goldenhomes2/footer.php
+++ wp-content/themes/redi-goldenhomes2/footer.php
@@ -8,7 +8,7 @@ Footer template
 <div class="footer">
 	<div class="container">		
 		<div class="row">
-			<div class="col-md-5">
+			<div class="col-sm-5">
 				<div class="footer_info">
 					<h2 class="tru_so_chinh">Trụ sở chính</h2>
 					<div class="address_r">
@@ -30,7 +30,7 @@ Footer template
 					</div>
 				</div>				
 			</div>
-			<div class="col-md-7">
+			<div class="col-sm-7">
 				<div class="footer_info san-pham-footer">
 					<h2 class="tru_so_chinh">Sản phẩm</h2>
 					<ul class="menu_footer">
@@ -47,7 +47,18 @@ Footer template
 					</ul>								
 				</div>
 				<div class="map-box">
-					<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="340" data-height="260" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+					<div class="map-taro">
+						<!--<div class="fb-page" data-href="https://www.facebook.com/Golden-Homes-242837869639266/" data-tabs="timeline" data-width="340" data-height="260" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Golden-Homes-242837869639266/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Golden-Homes-242837869639266/">Golden Homes</a></blockquote></div>-->
+						<a href="javascript:void(0);"><img src="<?php echo P_IMG.'/fanpage.png'; ?>"></a>
+					</div>					
+					<div class="box-social-map">
+						<ul class="footer_social">
+							<li><a href="javascript:void(0);" title="tiêu đề tên" target="_blank" rel="nofollow"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="javascript:void(0);" title="tiêu đề tên" target="_blank" rel="nofollow"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+							<li><a href="javascript:void(0);" title="tiêu đề tên" target="_blank" rel="nofollow"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="javascript:void(0);" title="tiêu đề tên" target="_blank" rel="nofollow"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+						</ul>	
+					</div>
 				</div>
 				<div class="clr"></div>						
 			</div>			
